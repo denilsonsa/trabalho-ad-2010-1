b397482730af67195e58ece95a081358a10deb36
Denilson Sá <denilsonsa@gmail.com>
2010-07-16 22:42:30 -0300

    Fase transiente do cenario_2.py aumentada de 4500000 para 5000000.

--- cenario_2.py
+++ cenario_2.py
@@ -42,7 +42,7 @@ def main():
         tempo_reforco_jam = 3.2,
         tempo_fatia_backoff = 51.2,
         tempo_propagacao = 0.005, #5 microseg/km = 0.005 microseg/m
-        eventos_fase_transiente = 4500000,
+        eventos_fase_transiente = 5000000,
         eventos_por_rodada = 250000
     )
 
